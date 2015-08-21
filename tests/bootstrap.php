50d9819b6b3b6c3a6fcb8d4c1d1e9196cf9f06c7
Michael Fairchild <mfairchild365@gmail.com>
2015-08-21 11:30:29 -0500

    Always require

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,7 @@ $autoloader = require __DIR__ . '/../vendor/autoload.php';
 $autoloader->add(__NAMESPACE__, __DIR__);
 
 if (file_exists(__DIR__ . '/config.inc.php')) {
-    $config = require_once __DIR__ . '/config.inc.php';
+    $config = require __DIR__ . '/config.inc.php';
     putenv('REV_CLIENT_API_KEY=' . $config['REV_CLIENT_API_KEY']);
     putenv('REV_USER_API_KEY=' . $config['REV_USER_API_KEY']);
 }
